64530daa0992a1d1a09b7fcaeafd294011b948c6
IrfanisMalay <meatballguy3@gmail.com>
2025-08-02 21:38:47 +0800

    radio and sound changes

--- main.ts
+++ main.ts
@@ -2,9 +2,11 @@ enum RadioMessage {
     message1 = 49434
 }
 radio.onReceivedMessage(RadioMessage.message1, function () {
-    music.play(music.stringPlayable("B B B - B - B B ", 360), music.PlaybackMode.UntilDone)
+    radio.setGroup(1)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.UntilDone)
 })
 radio.setFrequencyBand(2.4)
+radio.sendMessage(RadioMessage.message1)
 basic.forever(function () {
 	
 })
